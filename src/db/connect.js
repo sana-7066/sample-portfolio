8c3ecf3689669d31d104cecc846dc4acdb8883b1
sana u shaikh <hkiahs66@gmail.com>
2022-09-13 12:28:58 +0530

    connection changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.HOST,{useNewUrlParser:true, useUnifiedTopology : true})//,useCreateIndex : true
+mongoose.connect(process.env.HOST,{useNewUrlParser:true, useUnifiedTopology : true})
 .then(() => console.log('connection successful'))
 .catch((err) => console.log('Error :' +err));
